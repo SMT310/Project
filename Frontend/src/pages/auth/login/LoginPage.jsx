85ce9a69ceb1b32e832f005d2f88533cb94a5468
SMT310 <shinizeam@gmail.com>
2024-12-07 14:51:55 +0700

    Fix login sign up color

--- Frontend/src/pages/auth/login/LoginPage.jsx
+++ Frontend/src/pages/auth/login/LoginPage.jsx
@@ -56,12 +56,12 @@ const LoginPage = () => {
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
 			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className='lg:w-2/3 fill-white' />
+				<XSvg className='lg:w-2/3 fill-[#1DA1F2]' />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
-					<h1 className='text-4xl font-extrabold text-white'>Login now</h1>
+					<XSvg className='w-24 lg:hidden fill-[#1DA1F2]' />
+					<h1 className='text-4xl font-extrabold text-[#1DA1F2]'>Login now</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<IoIosMail />
 						<input
@@ -105,15 +105,19 @@ const LoginPage = () => {
 							/>
 						</label>
 					</div> */}
-					<button className='btn rounded-full btn-primary text-white'>
+					{/* <button className='btn rounded-full btn-primary text-white'> */}
+					<button className="btn rounded-full bg-[#1DA1F2] text-white btn-outline w-full">
 						{isPending ? "Loading..." : "Login"}
 					</button>
 					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
 				<div className='flex flex-col gap-2 mt-4'>
-					<p className='text-white text-lg'>{"Don't"} have an account?</p>
+					<p className='text-[#333333] text-lg'>{"Don't"} have an account?</p>
 					<Link to='/signup'>
-						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
+						{/* <button className='btn rounded-full btn-primary text-white btn-outline w-full'> */}
+						<button className="btn rounded-full bg-[#1DA1F2] text-white btn-outline w-full">
+							Sign up
+						</button>
 					</Link>
 				</div>
 			</div>
--- Frontend/src/pages/auth/signup/SignUpPage.jsx
+++ Frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -60,12 +60,12 @@ const SignUpPage = () => {
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
 			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className=' lg:w-2/3 fill-white' />
+				<XSvg className=' lg:w-2/3 fill-[#1DA1F2]' />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
-					<h1 className='text-4xl font-extrabold text-white'>Sign up today</h1>
+					<XSvg className='w-24 lg:hidden fill-[#1DA1F2]' />
+					<h1 className='text-4xl font-extrabold text-[#1DA1F2]'>Sign up today</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<IoIosMail />
 						<input
@@ -112,15 +112,15 @@ const SignUpPage = () => {
 							value={formData.password}
 						/>
 					</label>
-					<button className='btn rounded-full btn-primary text-white'>
+					<button className='btn rounded-full bg-[#1DA1F2] text-white btn-outline w-full'>
 						{isPending ? "Loading..." : "Sign up"}
 					</button>
 					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
-				<div className='flex flex-col lg:w-2/3 gap-2 mt-4'>
-					<p className='text-white text-lg'>Already have an account?</p>
+				<div className='flex flex-col gap-2 mt-4'>
+					<p className='text-[#333333] text-lg'>Already have an account?</p>
 					<Link to='/login'>
-						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign in</button>
+						<button className='btn rounded-full bg-[#1DA1F2] text-white btn-outline w-full'>Login</button>
 					</Link>
 				</div>
 			</div>
